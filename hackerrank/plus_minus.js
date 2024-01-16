0d2612b6bec12d203d491fae270e0859990b7306
Mihndim <mihndim2016@gmail.com>
2024-01-16 11:53:03 +0100

    Add second solution to PlusMinus function

--- hackerrank/plus_minus.js
+++ hackerrank/plus_minus.js
@@ -14,4 +14,29 @@ function plusMinus(arr) {
       zero++;
     }
   }
+  console.log((pos / arr.length).toFixed(6));
+  console.log((neg / arr.length).toFixed(6));
+  console.log((zero / arr.length).toFixed(6));
+}
+
+function plusMinus2(arr) {
+  let results = {
+    pos: 0,
+    neg: 0,
+    zero: 0,
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      results.pos++;
+    } else if (arr[i] < 0) {
+      results.neg++;
+    } else {
+      results.zero++;
+    }
+  }
+
+  for (let [key, value] of Object.entries(results)) {
+    console.log((value / arr.length).toFixed(6));
+  }
 }
