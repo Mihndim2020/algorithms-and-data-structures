5ab18dda624790fc0b6e22b07c66fec8a708a065
Mihndim <mihndim2016@gmail.com>
2024-01-20 21:57:47 +0100

    Add function grading students

--- hackerrank/timeConversion.js
+++ hackerrank/timeConversion.js
@@ -3,12 +3,11 @@ function timeConversion(s) {
   let toArray = s.split(":");
   console.log(toArray);
   if (s.slice(-2) === "AM") {
-    return `${parseInt(toArray[0]) % 12}:${toArray[1]}:${toArray[2].slice(
-      0,
-      2
-    )}`;
+    return `${(parseInt(toArray[0]) % 12).toString().padStart(2, "0")}:${
+      toArray[1]
+    }:${toArray[2].slice(0, 2)}`;
   } else {
-    return `${(parseInt(toArray[0]) % 12) + 12}:${
+    return `${((parseInt(toArray[0]) % 12) + 12).toString().padStart(2, "0")}:${
       toArray[1]
     }:${toArray[2].slice(0, 2)}`;
   }
