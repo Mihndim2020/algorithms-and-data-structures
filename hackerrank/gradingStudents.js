aa9ca294aace530a0f37f42f0403ce628c2f6b46
Mihndim <mihndim2016@gmail.com>
2024-01-20 22:00:08 +0100

    Implement function

--- hackerrank/gradingStudents.js
+++ hackerrank/gradingStudents.js
@@ -4,3 +4,14 @@
 //Sam is a professor at the university and likes to round each student's  according to these rules:
 //If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
 //If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+
+function gradingStudents(grades) {
+  let output = grades.map((elem) => {
+    if (elem % 5 >= 3 && elem >= 38) {
+      return elem + (5 - (elem % 5));
+    } else {
+      return elem;
+    }
+  });
+  return output;
+}
