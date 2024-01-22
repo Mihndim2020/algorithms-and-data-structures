ed6bf230863732baa0826f161d03565d5db4078a
Mihndim <mihndim2016@gmail.com>
2024-01-22 22:12:45 +0100

    Implement countElements function

--- hackerrank/countElement.js
+++ hackerrank/countElement.js
@@ -1,1 +1,11 @@
 // Write a function to counts for a given element x, the number of x+1 elements that exist in the array.
+
+const countElements = (arr) => {
+  let count = 0;
+  arr.forEach((element) => {
+    if (arr.includes(element + 1)) {
+      count += 1;
+    }
+  });
+  return count;
+};
