1ad4122060b3888348b3409180abf009a5763632
Mihndim <mihndim2016@gmail.com>
2024-02-28 04:11:50 +0100

    Add sortedObj array

--- hackerrank/sortArray.js
+++ hackerrank/sortArray.js
@@ -5,3 +5,17 @@ const sorted = (arr) => {
 };
 
 console.log(sorted(numbers));
+
+const books = [
+  { name: "Harry Potter", author: "Joanne Rowling" },
+  { name: "Warcross", author: "Marie Lu" },
+  { name: "The hunger games", author: "Suzanne Collins" },
+];
+
+const sortedObj = (arr) => {
+  return arr.sort((a, b) =>
+    a.author.split(" ")[1] < b.author.split(" ")[1] ? -1 : 1
+  );
+};
+
+console.log(sortedObj(books));
