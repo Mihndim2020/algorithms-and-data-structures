e6fb247231205726a2413241809c13ada230bdb3
Mihndim <mihndim2016@gmail.com>
2024-02-28 03:27:30 +0100

    Update removeDuplicate array function

--- hackerrank/removeDuplicates.js
+++ hackerrank/removeDuplicates.js
@@ -5,3 +5,21 @@ const removeDuplicates = (arr) => {
 const myArry = [1, 1, 2, 3, 4, 4, 5, 5];
 
 console.log(removeDuplicates(myArry));
+
+const removeDup = (arr) => {
+  return arr.reduce((acc, curr) => {
+    return acc.includes(curr) ? acc : [...acc, curr];
+  }, []);
+};
+console.log(removeDup(myArry));
+
+const removeDupl = (arr) => {
+  let singleton = [];
+  arr.forEach((elem) => {
+    if (!singleton.includes(elem)) {
+      singleton.push(elem);
+    }
+  });
+  return singleton;
+};
+console.log(removeDupl(myArry));
