e87ea4204c48581e8215d9aebdf9529ed074ad2d
Mihndim <mihndim2016@gmail.com>
2024-03-04 04:45:39 +0100

    Add asyncMapping functions

---
+++ hackerrank/mappingPromises.js
@@ -0,0 +1,23 @@
+const getUsers = () => {
+  setTimeout(() => {
+    console.log("Array of users");
+  });
+};
+
+const getStatus = () => {
+  setTimeout(() => {
+    console.log("Array of statuses");
+  });
+};
+
+getUsers().then((users) => {
+  getStatus().then((userStatuses) => {
+    const mappedUsers = users.map((user) => {
+      const isActive = userStatuses.find((userStatus) => {
+        userStatus.id === user.id;
+      }).isActive;
+      return { ...user, isActive };
+    });
+    console.log("mappedUsers", mappedUsers);
+  });
+});
