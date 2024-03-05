ba3ee5bc79a0701664c13be8489dac5642702375
Mihndim <mihndim2016@gmail.com>
2024-03-05 03:57:02 +0100

    Add async/await solution

---
+++ hackerrank/asyncAwait.js
@@ -0,0 +1,11 @@
+const getUsers = () => {
+  setTimeout(() => {
+    console.log("Array of users");
+  });
+};
+
+const getStatus = () => {
+  setTimeout(() => {
+    console.log("Array of statuses");
+  });
+};
