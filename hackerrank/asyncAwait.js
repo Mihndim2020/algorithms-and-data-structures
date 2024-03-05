dcce4ffd020fefef6240c60a27217a542c2d5df4
Mihndim <mihndim2016@gmail.com>
2024-03-05 04:04:15 +0100

    Update async/await function

--- hackerrank/asyncAwait.js
+++ hackerrank/asyncAwait.js
@@ -1,11 +1,25 @@
 const getUsers = () => {
   setTimeout(() => {
     console.log("Array of users");
-  });
+  }, 3000);
 };
 
 const getStatus = () => {
   setTimeout(() => {
     console.log("Array of statuses");
-  });
+  }, 2000);
 };
+
+const getMappedUsers = async () => {
+  const users = await getUsers();
+  const userStatuses = await getStatus();
+  const mappedUsers = users.map((user) => {
+    const isActive = userStatuses.find((userStatus) => {
+      userStatus.id === user.id;
+    }).isActive;
+    return { ...user, isActive };
+  });
+  console.log("MappedUsers", mappedUsers);
+};
+
+getMappedUsers();
