0aedf5cec9bbdcb8c1f7b3860c9a766f826adf95
Mihndim <mihndim2016@gmail.com>
2024-03-06 04:06:29 +0100

    Update requestManager

--- hackerrank/fetchFunction.js
+++ hackerrank/fetchFunction.js
@@ -1,14 +1,40 @@
-const requestManager = async (url, options = {}, attemps = 3) => {
-  let delay = 1000;
-  fetch(url).then((response) => {
-    if (attemps > 0 && !response) {
-      attemps -= 1;
-      delay *= 5;
-      setTimeout(() => {
-        requestManager(url, attemps);
-      }, delay);
-    } else {
-      return response;
-    }
+// const requestManager = async (url, options = {}, attemps = 3) => {
+//   let delay = 1000;
+//   fetch(url).then((response) => {
+//     if (attemps > 0 && !response) {
+//       attemps -= 1;
+//       delay *= 5;
+//       setTimeout(() => {
+//         requestManager(url, attemps);
+//       }, delay);
+//     } else {
+//       return response;
+//     }
+//   });
+// };
+
+const requestManager = (url, options = {}, attemps = 3) => {
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then(resolve)
+      .catch((error) => {
+        const isLastAttemp = attemps === 1;
+        if (isLastAttemp) {
+          return reject(error);
+        }
+        setTimeout(() => {
+          requestManager(url, options, attemps - 1)
+            .then(resolve)
+            .catch(reject);
+        }, 3000);
+      });
   });
 };
+
+requestManager("https://foo.com")
+  .then((response) => {
+    console.log("Response", response);
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
