b37814efec26bcd770763907d12a84a9f1ad70d5
Mihndim <mihndim2016@gmail.com>
2024-03-06 03:49:47 +0100

    Update fetch function

--- hackerrank/fetchFunction.js
+++ hackerrank/fetchFunction.js
@@ -1,14 +1,14 @@
-const fetchFunction = async (url, attemps) => {
+const requestManager = async (url, options = {}, attemps = 3) => {
   let delay = 1000;
   fetch(url).then((response) => {
     if (attemps > 0 && !response) {
       attemps -= 1;
       delay *= 5;
       setTimeout(() => {
-        fetchFunction(url, attemps);
+        requestManager(url, attemps);
       }, delay);
     } else {
-      console.log(response);
+      return response;
     }
   });
 };
