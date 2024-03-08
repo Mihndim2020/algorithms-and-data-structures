eef7e58019dc1e515af12cdb9fd931cc898aeaf5
Mihndim <mihndim2016@gmail.com>
2024-03-08 03:25:09 +0100

    Update memoizedFunction

--- hackerrank/memoizedFunction.js
+++ hackerrank/memoizedFunction.js
@@ -1,12 +1,21 @@
-const memoizedFunction = (input) => {
+const memoizeAdd = (input) => {
   let cache = {};
-  if (cache[input]) {
-    return cache[input];
-  }
-  cache[input] = 10 + input;
-  return cache[input];
+  return (value) => {
+    if (value in cache) {
+      console.log("Fetching results");
+      return cache[value];
+    } else {
+      console.log("Calculating results");
+      // const result = value + 10;
+      cache[value] = 10 + value;
+      return cache[value];
+    }
+  };
 };
 
-console.log(memoizedFunction(10));
-console.log(memoizedFunction(15));
-console.log(memoizedFunction(14));
+const newAdd = memoizeAdd();
+
+console.log(newAdd(10));
+console.log(newAdd(15));
+console.log(newAdd(14));
+console.log(newAdd(10));
