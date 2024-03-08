1a4cf07161dd0af2c3f37ce84550209a1d866e2e
Mihndim <mihndim2016@gmail.com>
2024-03-08 05:04:41 +0100

    Add anagrams

---
+++ hackerrank/anagram.js
@@ -0,0 +1,9 @@
+const isAnagram = (str1, str2) => {
+  return (
+    str1.toLowerCase().split("").sort().join("") ===
+    str2.toLowerCase().split("").sort().join("")
+  );
+};
+
+console.log(isAnagram("listen", "silent"));
+console.log(isAnagram("listen", "talk"));
