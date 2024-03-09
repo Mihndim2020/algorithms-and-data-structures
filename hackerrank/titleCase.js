7f9af0f209446467f94ca7d8dbdd0e29e31de3b6
Mihndim <mihndim2016@gmail.com>
2024-03-09 10:12:58 +0100

    Add covertTo24hours function

--- hackerrank/titleCase.js
+++ hackerrank/titleCase.js
@@ -1,8 +1,12 @@
 const toTitleCase = (str) => {
   let titleCase = "";
-  return str.split(" ").map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(0, word.length + 1);
-  });
+  return str
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join(" ");
 };
 
 console.log(toTitleCase("the game changers and the change makers"));
+console.log(toTitleCase("HERE IS MY HANDLE, HERE IS MY SPOUT"));
