98326285da53310df761155cde1399c09491171d
Mihndim <mihndim2016@gmail.com>
2024-03-11 04:01:13 +0100

    Add replaceParams function

---
+++ hackerrank/paramReplace.js
@@ -0,0 +1,13 @@
+const replaceParamsUrl = (initialUrl, arr) => {
+  console.log(initialUrl.split(":"));
+  console.log(arr);
+};
+
+const initialUrl = "/posts/:postId/comments/:commentId";
+
+const resultUrl = replaceParamsUrl(initialUrl, [
+  { from: "postId", to: "1" },
+  { from: "commentId", to: "3" },
+]);
+
+console.log(resultUrl);
